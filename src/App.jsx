3323d3fb965f1492d65894e5592065aafb368260
Jan <janninh@gmx.de>
2025-11-26 14:23:48 +0100

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <button className="btn btn-secondary"> Click me! </button>
+        Test
+      </div>
+    </>
+  );
+}
+
+export default App;
